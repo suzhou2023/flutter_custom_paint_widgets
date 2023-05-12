9d0e5489503893c9cbfcd96952be567d5d569ee6
suzhou <withwind318@gmail.com>
2023-05-12 09:51:34 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'paper.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(Paper());
+}
---
+++ lib/paper.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Paper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: CustomPaint(
+        painter: PaperPainter(),
+      ),
+    );
+  }
+}
+
+class PaperPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint();
+    canvas.drawCircle(const Offset(100, 100), 10, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
